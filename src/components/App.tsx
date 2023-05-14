a8aa1a8f323645738dfe3ffbfa95f86b843bd16c
ciberdown <teymuri.amin@gmail.com>
2023-05-14 19:21:17 +0330

    ready to write codes

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import '../../src/App.css';
+import Button from 'react-bootstrap/Button';
+function App() {
+  return (
+    <div className="App">
+      amin teymuri
+      <Button>click</Button>
+    </div>
+  );
+}
+
+export default App;
