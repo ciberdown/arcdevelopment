096f70b9cb07fe6069ffd196129b3b06ca020407
ciberdown <teymuri.amin@gmail.com>
2023-05-16 13:41:37 +0330

    slider removed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,16 +4,12 @@ import theme from "./UI/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import { CacheProvider } from "@emotion/react";
 import { cacheRtl } from "./UI/functions";
-import { Slider } from "@mui/material";
 function App() {
   console.log(theme)
-  const sliderHandle = (e: any)=>{
-    console.log(e.target.value)
-  }
+
   return (
     <CacheProvider value={cacheRtl} >
       <ThemeProvider theme={theme}>
-        <Slider sx={{m:10, width:200}} color= {}  onChange={sliderHandle}/>
         <NavBar>
           <></>
         </NavBar>
