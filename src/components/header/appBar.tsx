ecb060b99bdc676ba69d1e105a6982a227f6bed2
ciberdown <teymuri.amin@gmail.com>
2023-05-21 22:26:01 +0330

    reImplementation complete

--- src/components/header/appBar.tsx
+++ src/components/header/appBar.tsx
@@ -1,18 +1,77 @@
 import Toolbar from "@mui/material/Toolbar";
 import AppBar from "@mui/material/AppBar";
 import { ElevationScroll, ElevationProps } from "../UI/functions";
+import ColorTabs from "./tabs";
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import { useNavigate } from "react-router";
+import { styles } from "../UI/styles";
+import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import logo from "../../assets/logo.svg";
 import { myStyles } from "../UI/styles";
-import Tabs from "./tabs";
+import ServicesMenu from "../body/services/servicesMenu";
 
-import { NavigateFunction, useNavigate } from "react-router";
 export default function NavBar(props: ElevationProps) {
-  const navigate: NavigateFunction = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [value, setValue] = useState("one");
   const classes = myStyles();
+  const myPathes = {
+    one: "/",
+    two: "/services",
+    three: "/revelotion",
+    four: "/about",
+    five: "/contact",
+  };
+  const entries = Object.entries(myPathes); //[['one', '/'], ['two', '/services'] ,...]
+  const navigate = useNavigate();
+  useEffect(() => {
+    //when page reloads
+    const path: string = window.location.pathname; // '/'
+    entries.forEach((item) => {
+      if (path === item[1]) {
+        setValue(item[0]);
+        return; //break
+      } else if (path === "/freeEstimate") {
+        setValue("free");
+      }
+    });
+  }, [value]);
+
   return (
     <ElevationScroll {...props}>
       <AppBar position="fixed">
         <Toolbar disableGutters={true} className={classes.toolbarMargin}>
-          <Tabs />
+          <img
+            onClick={() => {
+              navigate("/");
+              setValue("one");
+            }}
+            className={classes.logo}
+            src={logo}
+            alt="arc logo"
+          />
+          <ColorTabs
+            setValue={setValue}
+            valueSetter={value}
+            entries={entries}
+            anchorEl={anchorEl}
+            setAnchorEl={setAnchorEl}
+          />
+          <Button
+            variant="contained"
+            sx={styles.button}
+            color="secondary"
+            onClick={() => {
+              navigate("/freeEstimate");
+              setValue("free");
+            }}
+          >
+            Free Estimated
+          </Button>
+          <ServicesMenu anchorEl={anchorEl} setAnchorEl={setAnchorEl} />
         </Toolbar>
       </AppBar>
     </ElevationScroll>
--- src/components/header/tabs.tsx
+++ src/components/header/tabs.tsx
@@ -1,41 +1,23 @@
-import * as React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import { useNavigate } from "react-router";
 import { styles } from "../UI/styles";
-import { useEffect, useState } from "react";
-import { Button } from "@mui/material";
-import logo from "../../assets/logo.svg";
-import { myStyles } from "../UI/styles";
-import ServicesMenu from "../body/services/servicesMenu";
-
-export default function ColorTabs() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [value, setValue] = useState("one");
-  const classes = myStyles();
-  const myPathes = {
-    one: "/",
-    two: "/services",
-    three: "/revelotion",
-    four: "/about",
-    five: "/contact",
-  };
-  const entries = Object.entries(myPathes); //[['one', '/'], ['two', '/services'] ,...]
+interface Props {
+  setValue: Function;
+  valueSetter: string;
+  entries: [string, string][];
+  anchorEl: HTMLElement | null;
+  setAnchorEl: Function;
+}
+export default function ColorTabs({
+  setValue,
+  valueSetter: value,
+  entries,
+  anchorEl,
+  setAnchorEl,
+}: Props) {
   const navigate = useNavigate();
-  useEffect(() => {
-    //when page reloads
-    const path: string = window.location.pathname; // '/'
-    entries.forEach((item) => {
-      if (path === item[1]) {
-        setValue(item[0]);
-        return; //break
-      } else if (path === "/freeEstimate") {
-        setValue("free");
-      }
-    });
-  }, [value]);
-
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
     entries.forEach((item) => {
@@ -48,18 +30,8 @@ export default function ColorTabs() {
   const handleClickServices = (event: any) => {
     setAnchorEl(event.currentTarget);
   };
-
   return (
     <>
-      <img
-        onClick={() => {
-          navigate("/");
-          setValue("one");
-        }}
-        className={classes.logo}
-        src={logo}
-        alt="arc logo"
-      />
       <Box sx={{ width: "100%", color: "red" }}>
         <Tabs
           value={value}
@@ -84,18 +56,6 @@ export default function ColorTabs() {
           <Tab sx={styles.tab} value="five" label="Contact us" />
         </Tabs>
       </Box>
-      <Button
-        variant="contained"
-        sx={styles.button}
-        color="secondary"
-        onClick={() => {
-          navigate("/freeEstimate");
-          setValue("free");
-        }}
-      >
-        Free Estimated
-      </Button>
-      <ServicesMenu anchorEl={anchorEl} setAnchorEl={setAnchorEl} />
     </>
   );
 }
